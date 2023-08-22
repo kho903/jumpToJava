3b1df0b225e25e4e7ff9071c6e22620b114af00b
Kim Ji Hun <gmldnr2222@naver.com>
2023-08-22 09:03:35 +0900

    feat: Animal 클래스

---
+++ src/main/java/chap05/_2class/animal/Sample.java
@@ -0,0 +1,22 @@
+package chap05._2class.animal;
+
+class Animal {
+	String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+public class Sample {
+	public static void main(String[] args) {
+		Animal cat = new Animal();
+		cat.setName("boby");
+
+		Animal dog = new Animal();
+		dog.setName("happy");
+
+		System.out.println(cat.name);
+		System.out.println(dog.name);
+	}
+}
